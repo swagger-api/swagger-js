0125cda7ba8b4836bc020052a04ad2f7dd287b1c
Tony Tam <fehguy@gmail.com>
2015-01-27 22:35:03 -0800

    added request tests

--- test/request.js
+++ test/request.js
@@ -1,4 +1,5 @@
 var test = require('unit.js')
+var expect = require('expect')
 var should = require('should')
 var mock = require('../test/mock');
 var swagger = require('../lib/swagger-client');
@@ -24,8 +25,76 @@ describe('api key authorizations', function() {
     var req = petApi.getPetById({petId: 1}, {mock: true});
 
     test.object(req);
-    should(req.method).equal('GET');
-    should(req.headers['Accept']).equal('application/json');
-    should(req.url).equal('http://localhost:8000/api/pet/1');
+    expect(req.method).toBe('GET');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.url).toBe('http://localhost:8000/api/pet/1');
+  });
+
+  it('generate a get request with query params', function() {
+    var petApi = sample.pet;
+    var req = petApi.findPetsByTags({tags: ["tag 1", "tag 2"]}, {mock: true});
+
+    test.object(req);
+    expect(req.method).toBe('GET');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.url).toBe('http://localhost:8000/api/pet/findByTags?tags=tag%201&tags=tag%202');
+  });
+
+  it('generate a get request with email query param array', function() {
+    var petApi = sample.pet;
+    var req = petApi.findPetsByStatus({status: ["fehguy@gmail.com", "nada"]}, {mock: true});
+
+    test.object(req);
+    expect(req.method).toBe('GET');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.url).toBe('http://localhost:8000/api/pet/findByStatus?status=fehguy%40gmail.com|nada');
+  });
+
+  it('generate a POST request with body', function() {
+    var petApi = sample.pet;
+    var req = petApi.addPet({body: {id: 100, name: 'gorilla'}}, {mock: true});
+
+    test.object(req);
+    expect(req.method).toBe('POST');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.headers['Content-Type']).toBe('application/json');
+    expect(req.url).toBe('http://localhost:8000/api/pet');
+    expect(req.body).toEqual({ id: 100, name: 'gorilla' });
+  });
+
+  it('generate a POST request with no body', function() {
+    var petApi = sample.pet;
+    var req = petApi.addPet({}, {mock: true});
+
+    test.object(req);
+    expect(req.method).toBe('POST');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.headers['Content-Type']).toBeUndefined;
+    expect(req.url).toBe('http://localhost:8000/api/pet');
+    expect(req.body).toBeUndefined;
+  });
+
+  it('generate a POST request with an empty body', function() {
+    var petApi = sample.pet;
+    var req = petApi.addPet({body:{}}, {mock: true});
+
+    test.object(req);
+    expect(req.method).toBe('POST');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.headers['Content-Type']).toBe('application/json');
+    expect(req.url).toBe('http://localhost:8000/api/pet');
+    expect(req.body).toEqual({});
+  });
+
+  it('generate a POST request for application/x-www-form-urlencoded', function() {
+    var petApi = sample.pet;
+    var req = petApi.updatePetWithForm({petId:100, name: 'monster', status: 'miserable dog'}, {mock: true});
+
+    test.object(req);
+    expect(req.method).toBe('POST');
+    expect(req.headers.Accept).toBe('application/json');
+    expect(req.headers['Content-Type']).toBe('application/x-www-form-urlencoded');
+    expect(req.url).toBe('http://localhost:8000/api/pet/100');
+    expect(req.body).toEqual('name=monster&status=miserable%20dog');
   });
 });
