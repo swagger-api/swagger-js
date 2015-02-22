209019f1390f1652926a9446398244ccef2f7976
Tony Tam <fehguy@gmail.com>
2015-02-21 23:46:09 -0800

    added malformed link tests

--- test/resolver.js
+++ test/resolver.js
@@ -31,15 +31,64 @@ describe('swagger resolver', function() {
       definitions: {
         Pet: {
           properties: {
-            category: {
-              $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Category'
-            }
+            category: { $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Category' }
           }
         }
       }
     };
     api.resolve(spec, function(spec, unresolvedRefs) {
       expect(spec.definitions.Category).toExist();
+      done();
+    });
+  });
+
+  it('doesn\'t die on a broken remote model property reference', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      definitions: {
+        Pet: {
+          properties: {
+            category: { $ref: 'http://localhost:8000/v2/petstore.jsonZZZ#/definitions/Category' }
+          }
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      expect(unresolvedRefs['http://localhost:8000/v2/petstore.jsonZZZ#/definitions/Category']).toBe(null);
+      done();
+    });
+  });
+
+  it('doesn\'t die on a broken remote model property reference path', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      definitions: {
+        Pet: {
+          properties: {
+            category: { $ref: 'http://localhost:8000/v2/petstore.json#/definitionz/Category' }
+          }
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      expect(unresolvedRefs['http://localhost:8000/v2/petstore.json#/definitionz/Category']).toBe(null);
+      done();
+    });
+  });
+
+  it('doesn\'t die on a broken remote model property reference path 2', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      definitions: {
+        Pet: {
+          properties: {
+            category: { $ref: 'http://localhost:8000/v2/petstore.json#/definition/Categoryzzz' }
+          }
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      expect(unresolvedRefs['http://localhost:8000/v2/petstore.json#/definition/Categoryzzz']).toBe(null);
       done();
     });
   });
@@ -52,9 +101,7 @@ describe('swagger resolver', function() {
           properties: {
             category: {
               type: 'array',
-              items: {
-                $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Category'
-              }
+              items: { $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Category' }
             }
           }
         }
@@ -76,9 +123,7 @@ describe('swagger resolver', function() {
               in: 'body',
               name: 'body',
               required: false,
-              schema: {
-                $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Pet'
-              }
+              schema: { $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Pet' }
             }]
           }
         }
@@ -96,13 +141,10 @@ describe('swagger resolver', function() {
       paths: {
         '/pet': {
           post: {
-            parameters: [],
             responses: {
               200: {
                 description: 'it worked!',
-                schema: {
-                  $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Pet'
-                }
+                schema: { $ref: 'http://localhost:8000/v2/petstore.json#/definitions/Pet' }
               }
             }
           }
@@ -112,6 +154,128 @@ describe('swagger resolver', function() {
     api.resolve(spec, function(spec, unresolvedRefs) {
       expect(spec.definitions.Pet).toExist();
       expect(spec.paths['/pet'].post.responses['200'].schema.$ref).toBe('#/definitions/Pet');
+      done();
+    });
+  });
+
+  it('resolves a locally defined parameter $ref', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      paths: {
+        '/pet': {
+          post: {
+            parameters: [{
+              $ref: '#/parameters/sharedSkip'
+            }]
+          }
+        }
+      },
+      parameters: {
+        sharedSkip: {
+          name: "skip",
+          in: "query",
+          description: "Results to skip",
+          required: false,
+          type: "integer",
+          format: "int32"
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      var params = spec.paths['/pet'].post.parameters;
+      expect(params.length).toBe(1);
+      var param = params[0];
+      expect(param.name).toBe('skip'); 
+      done();
+    });
+  });
+
+  it('doesn\'t puke on a malformed locally defined parameter $ref', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      paths: {
+        '/pet': {
+          post: {
+            parameters: [{
+              $ref: '#/parameters/sharedSkipz'
+            }]
+          }
+        }
+      },
+      parameters: {
+        sharedSkip: {
+          name: "skip",
+          in: "query",
+          description: "Results to skip",
+          required: false,
+          type: "integer",
+          format: "int32"
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      expect(unresolvedRefs['#/parameters/sharedSkipz']).toBe(null);
+      done();
+    });
+  });
+
+  it('resolves a remote defined parameter $ref', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      paths: {
+        '/pet': {
+          post: {
+            parameters: [{
+              $ref: 'http://localhost:8000/v2/petstore.json#/parameters/sharedSkip'
+            }]
+          }
+        }
+      },
+      parameters: {
+        sharedSkip: {
+          name: "skip",
+          in: "query",
+          description: "Results to skip",
+          required: false,
+          type: "integer",
+          format: "int32"
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      var params = spec.paths['/pet'].post.parameters;
+      expect(params.length).toBe(1);
+      var param = params[0];
+      expect(param.name).toBe('skip'); 
+      done();
+    });
+  });
+
+  it('doesn\'t puke on a malformed remote defined parameter $ref', function(done) {
+    var api = new swagger.Resolver();
+    var spec = {
+      paths: {
+        '/pet': {
+          post: {
+            parameters: [{
+              $ref: 'http://localhost:8000/v2/petstore.json#/parameters/sharedSkipz'
+            }]
+          }
+        }
+      },
+      parameters: {
+        sharedSkip: {
+          name: "skip",
+          in: "query",
+          description: "Results to skip",
+          required: false,
+          type: "integer",
+          format: "int32"
+        }
+      }
+    };
+    api.resolve(spec, function(spec, unresolvedRefs) {
+      expect(unresolvedRefs['http://localhost:8000/v2/petstore.json#/parameters/sharedSkipz']).toBe(null);
       done();
     });
   });
