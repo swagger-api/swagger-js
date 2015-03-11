c4cdc4870f1efdf26f7e6d722e2f65a2c7a6eccf
Jeremy Whitlock <jwhitlock@apache.org>
2015-03-11 11:13:13 -0600

    Add response properties to operation responses
    Fixes #277

--- test/responses.js
+++ test/responses.js
@@ -49,4 +49,73 @@ describe('response types', function () {
     expect(typeof responses['201']).toBe('object');
     expect(typeof responses['default']).toBe('object');
   });
+
+  it('should contain expected response properties (Issue 277)', function () {
+    var description = 'Test description';
+    var examples = {
+      'application/json': {
+        name: 'Anonymous'
+      }
+    };
+    var headers = {
+      'X-Testing': {
+        type: 'string'
+      }
+    };
+    var schema = {
+      properties: {
+        name: {
+          type: 'string'
+        }
+      }
+    };
+    var responses = {
+      '200': {
+        description: description,
+        examples: examples,
+        headers: headers,
+        schema: schema
+      },
+      'default': {
+        description: description,
+        examples: {
+          'application/json': {
+            id: 1,
+            name: 'Test Monster'
+          }
+        },
+        headers: headers,
+        schema: {
+          $ref: '#/definitions/Monster'
+        }
+      }
+    };
+    var definitions = {
+      Monster: MonsterModel
+    };
+
+    responses['default'].schema = {
+      $ref: '#/definitions/Monster'
+    };
+    
+    var op = new Operation(
+      {},
+      'http',
+      'operationId',
+      'get',
+      '/path',
+      {responses: responses},
+      definitions);
+
+    // responses['200'] seems to be deleted by the Operation so we have to test differently
+    expect(op.successResponse['200'].definition).toEqual(schema);
+    expect(op.successResponse['200'].description).toBe(description);
+    expect(op.successResponse['200'].examples).toEqual(examples);
+    expect(op.successResponse['200'].headers).toEqual(headers);
+
+    expect(op.responses['default'].description).toBe(responses['default'].description);
+    expect(op.responses['default'].examples).toEqual(responses['default'].examples);
+    expect(op.responses['default'].headers).toEqual(responses['default'].headers);
+    expect(op.responses['default'].schema).toEqual(responses['default'].schema);
+  });
 });
