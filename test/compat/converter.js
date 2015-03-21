c3126918ea6e6cbf6f5002d8de387d4ddda3b154
Tony Tam <fehguy@gmail.com>
2015-03-20 21:04:13 -0700

    added responses

--- test/compat/converter.js
+++ test/compat/converter.js
@@ -42,6 +42,51 @@ describe('converts specs', function () {
       var converter = new SwaggerSpecConverter();
       converter.setDocumentationLocation('http://localhost:8000/v1/api-docs');
       converter.convert(data.obj, function(swagger) {
+
+        // metadata tests
+        expect(swagger.swagger).toBe('2.0');
+        test.object(swagger.info);
+        var info = swagger.info;
+        test.string(info.description);
+        test.string(info.title);
+        test.string(info.termsOfService);
+        test.object(swagger.license);
+        expect(swagger.license.name).toBe('Apache 2.0');
+        expect(swagger.license.url).toBe('http://www.apache.org/licenses/LICENSE-2.0.html');
+
+        // paths
+        expect(Object.keys(swagger.paths).length).toBe(16);
+
+        // getPet
+        var getPet = swagger.paths['/pet/{petId}'].get;
+        expect(getPet.parameters.length).toBe(1);
+        
+        var param = getPet.parameters[0];
+        expect(param.name).toBe('petId');
+        expect(param.description).toBe('ID of pet that needs to be fetched');
+        expect(param.in).toBe('path');
+        expect(param.minimum).toBe("1.0");
+        expect(param.maximum).toBe("100000.0");
+        expect(param.type).toBe('integer');
+        expect(param.format).toBe('int64');
+
+        // responses
+        var responses = getPet.responses;
+        expect(Object.keys(responses).length).toBe(3);
+
+        expect(responses['200'].schema.$ref).toBe('#/definitions/Pet');
+        expect(responses['400'].description).toBe('Invalid ID supplied');
+        expect(responses['404'].description).toBe('Pet not found');
+
+        // models
+        var definitions = swagger.definitions;
+        expect(Object.keys(definitions).length).toBe(8)
+        var pet = definitions.Pet;
+        expect(Object.keys(pet.properties).length).toBe(8);
+        var photos = pet.properties['photoUrls'];
+        expect(photos.type).toBe('array');
+        test.object(photos.items);
+        expect(photos.items.type).toBe('string');
         done();
       });
     };
@@ -60,7 +105,51 @@ describe('converts specs', function () {
       var converter = new SwaggerSpecConverter();
       converter.setDocumentationLocation('http://localhost:8000/v1/api-docs');
       converter.convert(data.obj, function(swagger) {
-        console.log(JSON.stringify(swagger.paths["/pet/{petId}"].get, null, 2));
+
+        // metadata tests
+        expect(swagger.swagger).toBe('2.0');
+        test.object(swagger.info);
+        var info = swagger.info;
+        test.string(info.description);
+        test.string(info.title);
+        test.string(info.termsOfService);
+        test.object(swagger.license);
+        expect(swagger.license.name).toBe('Apache 2.0');
+        expect(swagger.license.url).toBe('http://www.apache.org/licenses/LICENSE-2.0.html');
+
+        // paths
+        expect(Object.keys(swagger.paths).length).toBe(5);
+
+        // getPet
+        var getPet = swagger.paths['/pet/{petId}'].get;
+        expect(getPet.parameters.length).toBe(1);
+        
+        var param = getPet.parameters[0];
+        expect(param.name).toBe('petId');
+        expect(param.description).toBe('ID of pet that needs to be fetched');
+        expect(param.in).toBe('path');
+        expect(param.minimum).toBe("1.0");
+        expect(param.maximum).toBe("100000.0");
+        expect(param.type).toBe('integer');
+        expect(param.format).toBe('int64');
+
+        // responses
+        var responses = getPet.responses;
+        expect(Object.keys(responses).length).toBe(3);
+
+        expect(responses['200'].schema.$ref).toBe('#/definitions/Pet');
+        expect(responses['400'].description).toBe('Invalid ID supplied');
+        expect(responses['404'].description).toBe('Pet not found');
+
+        // models
+        var definitions = swagger.definitions;
+        expect(Object.keys(definitions).length).toBe(3);
+        var pet = definitions.Pet;
+        expect(Object.keys(pet.properties).length).toBe(6);
+        var photos = pet.properties['photoUrls'];
+        expect(photos.type).toBe('array');
+        test.object(photos.items);
+        expect(photos.items.type).toBe('string');
         done();
       });
     };
@@ -79,7 +168,23 @@ describe('converts specs', function () {
       var converter = new SwaggerSpecConverter();
       converter.setDocumentationLocation('http://localhost:8000/v1/word.json');
       converter.convert(data.obj, function(swagger) {
-        // console.log(JSON.stringify(swagger, null, 2));
+        expect(Object.keys(swagger.paths).length).toBe(12);
+
+        var getDefinitions = swagger.paths['/word.{format}/{word}/definitions'].get;
+
+        // console.log(JSON.stringify(getDefinitions, null, 2));
+        expect(getDefinitions.parameters.length).toBe(7);
+        var sourceDictionaries = getDefinitions.parameters[4];
+        expect(sourceDictionaries.in).toBe('query');
+        expect(sourceDictionaries.type).toBe('array');
+        test.object(sourceDictionaries.items);
+        expect(sourceDictionaries.items.type).toBe('string');
+        test.array(sourceDictionaries['enum']);
+
+        var response200 = getDefinitions.responses['200'];
+        expect(response200.schema.type).toBe('array');
+        expect(response200.schema.items.$ref).toBe('#/definitions/Definition');
+
         done();
       });
     };
