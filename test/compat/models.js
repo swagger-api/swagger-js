17f44b3a4cfdd0a8455395d07bafe8f015175e12
Tony Tam <fehguy@gmail.com>
2015-05-13 07:19:49 -0400

    removed some cruft

--- test/compat/models.js
+++ test/compat/models.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var mock = require('./mock');
+var expect = require('expect');
 var sample, instance;
 
 describe('1.2 get model operations', function () {
@@ -18,46 +19,7 @@ describe('1.2 get model operations', function () {
 
     done();
   });
-/*
-  it('verifies the Pet model', function (done) {
-    var pet = sample.models.Pet;
 
-    expect(pet.name).toBe('Pet');
-
-    var props = pet.properties;
-
-    expect(props[0].name).toBe('id');
-    console.log(props[0]);
-    expect(props[0].dataType).toBe('integer');
-
-    expect(props[1].name).toBe('category');
-    expect(props[1].dataType).toBe('Category');
-
-    expect(props[2].name).toBe('name');
-    expect(props[2].dataType).toBe('string');
-
-    expect(props[3].name).toBe('photoUrls');
-    expect(props[3].dataType).toBe('array');
-    expect(props[3].refDataType).toBe('string');
-
-    expect(props[4].name).toBe('tags');
-    expect(props[4].dataType).toBe('array');
-    expect(props[4].refDataType).toBe('Tag');
-
-    expect(props[5].name).toBe('status');
-    expect(props[5].dataType).toBe('string');
-
-    done();
-  });
-
-  it('verifies the getAbsoluteBasePath method for relativeBasePath "/"', function (done) {
-    sample.basePath = 'http://localhost:8000/api-docs/';
-
-    expect(sample.pet.getAbsoluteBasePath('/')).toBe('http://localhost:8000');
-
-    done();
-  });
-*/
   it('doesn\'t add double slashes per #202', function (done) {
     sample.basePath = 'http://localhost:8000/api-docs/';
     
@@ -65,6 +27,13 @@ describe('1.2 get model operations', function () {
 
     petApi.getPetById({petId: 1}, {mock: true});
 
+    done();
+  });
+
+  it('verifies enum values in a model property', function(done) {
+    var user = sample.models.User;
+    expect(user.definition.properties.userStatus['enum']).toEqual([ '1-registered', '2-active', '3-closed' ]);
+    console.log(user.definition['enum']);
     done();
   });
 });
