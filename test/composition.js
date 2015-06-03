07c29996eb6e974db27cf444b8f2191849902fdb
Tony Tam <fehguy@gmail.com>
2015-06-03 16:49:16 -0700

    added composition logic

--- test/composition.js
+++ test/composition.js
@@ -9,7 +9,7 @@ var Resolver = require('../lib/resolver');
 var instance;
 
 describe('swagger resolver', function () {
-
+/*
   before(function (done) {
     mock.petstore(done, function (petstore, server){
       instance = server;
@@ -20,57 +20,98 @@ describe('swagger resolver', function () {
     instance.close();
     done();
   });
-
+*/
   it('resolves a model with composition', function (done) {
     var api = new Resolver();
     var spec = {
       definitions: {
         Animal : {
-          type: "object",
+          type: 'object',
+          required: [ 'name' ],
           properties : {
             name : {
-              type : "string"
+              type : 'string'
             },
             type : {
-              type : "string"
+              type : 'string'
             }
           },
-          discriminator : "type"
+          discriminator : 'type'
         },
         Human : {
           allOf : [ {
-            $ref : "#/definitions/Animal"
-          }, {
-            type: "object",
+            type: 'object',
             properties : {
               firstName : {
-                type : "string"
+                type : 'string'
               },
               lastName : {
-                type : "string"
+                type : 'string'
               }
             }
+          },
+          {
+            $ref : '#/definitions/Animal'
           } ]
         },
         Pet : {
           allOf : [ {
-            $ref : "#/definitions/Animal"
+            $ref : '#/definitions/Animal'
           }, {
-            type: "object",
-            required : [ "isDomestic", "name", "type" ],
+            type: 'object',
+            required : [ 'isDomestic', 'name', 'type' ],
             properties : {
               isDomestic : {
-                type : "boolean",
+                type : 'boolean',
                 default : false
               }
             }
           } ]
+        },
+        Monster: {
+          allOf : [
+            {
+              $ref: '#/definitions/Animal'
+            }, {
+              $ref: '#/definitions/Ghoul'
+            }, {
+              properties: {
+                hasScales: {
+                  type: 'boolean'
+                }
+              }
+            }
+          ]
+        },
+        Ghoul: {
+          required: [ 'fangs' ],
+          properties: {
+            fangs: {
+              type: 'integer',
+              format: 'int32'
+            }
+          }
         }
       }
     };
 
-    api.resolve(spec, 'http://localhost:8000/v2/petstore.json', function (spec) {
-      console.log(spec);
+    api.resolve(spec, 'http://localhost:8000/v2/petstore.json', function (spec, unresolved) {
+      test.object(spec.definitions.Monster);
+      var properties = spec.definitions.Monster.properties;
+      test.object(properties.name);
+      expect(properties.name['x-resolved-from']).toBe('#/definitions/Animal');
+      test.object(properties.type);
+      expect(properties.type['x-resolved-from']).toBe('#/definitions/Animal');
+      test.object(properties.firstName);
+      expect(properties.firstName['x-resolved-from']).toBe('#/definitions/Animal');
+      test.object(properties.lastName);
+      expect(properties.lastName['x-resolved-from']).toBe('#/definitions/Animal');
+      test.object(properties.isDomestic);
+      expect(properties.isDomestic['x-resolved-from']).toBe('#/definitions/Animal');
+      test.object(properties.fangs);
+      expect(properties.fangs['x-resolved-from']).toBe('#/definitions/Ghoul');
+      test.object(properties.hasScales);
+      expect(properties.hasScales['x-resolved-from']).toBe('self');
       done();
     });
   });
@@ -80,97 +121,97 @@ describe('swagger resolver', function () {
     var spec = {
       definitions: {
         Pet: {
-          discriminator: "petType",
+          discriminator: 'petType',
           properties: {
             name: {
-              type: "string"
+              type: 'string'
             },
             petType: {
-              type: "string"
+              type: 'string'
             }
           },
           required: [
-            "name",
-            "petType"
+            'name',
+            'petType'
           ]
         },
         Cat: {
-          description: "A representation of a cat",
+          description: 'A representation of a cat',
           allOf: [
             {
-              $ref: "#/definitions/Pet"
+              $ref: '#/definitions/Pet'
             },
             {
               properties: {
                 petType: {
-                  type: "string",
-                  enum: [ "cat" ]
+                  type: 'string',
+                  enum: [ 'cat' ]
                 },
                 huntingSkill: {
-                  type: "string",
-                  description: "The measured skill for hunting",
-                  default: "lazy",
+                  type: 'string',
+                  description: 'The measured skill for hunting',
+                  default: 'lazy',
                   enum: [
-                    "clueless",
-                    "lazy",
-                    "adventurous",
-                    "aggressive"
+                    'clueless',
+                    'lazy',
+                    'adventurous',
+                    'aggressive'
                   ]
                 }
               },
               required: [
-                "huntingSkill"
+                'huntingSkill'
               ]
             }
           ]
         },
         Dog: {
-          description: "A representation of a dog",
+          description: 'A representation of a dog',
           allOf: [
             {
-              $ref: "#/definitions/Pet"
+              $ref: '#/definitions/Pet'
             },
             {
               properties: {
                 petType: {
-                  type: "string",
-                  enum: [ "dog" ]
+                  type: 'string',
+                  enum: [ 'dog' ]
                 },                
                 packSize: {
-                  type: "integer",
-                  format: "int32",
-                  description: "the size of the pack the dog is from",
+                  type: 'integer',
+                  format: 'int32',
+                  description: 'the size of the pack the dog is from',
                   default: 0,
                   minimum: 0
                 }
               },
               required: [
-                "packSize"
+                'packSize'
               ]
             }
           ]
         },
         Fish: {
-          description: "A representation of a fish",
+          description: 'A representation of a fish',
           allOf: [
             {
-              $ref: "#/definitions/Pet"
+              $ref: '#/definitions/Pet'
             },
             {
               properties: {
                 petType: {
-                  type: "string",
-                  enum: [ "fish" ]
+                  type: 'string',
+                  enum: [ 'fish' ]
                 },                
                 fins: {
-                  type: "integer",
-                  format: "int32",
-                  description: "count of fins",
+                  type: 'integer',
+                  format: 'int32',
+                  description: 'count of fins',
                   minimum: 0
                 }
               },
               required: [
-                "fins"
+                'fins'
               ]
             }
           ]
@@ -178,11 +219,10 @@ describe('swagger resolver', function () {
       }
     };
 
-    api.resolve(spec, 'http://localhost:8000/v2/petstore.json', function (spec) {
-      console.log(spec);
+    api.resolve(spec, 'http://localhost:8000/v2/petstore.json', function (spec, unresolved) {
+      console.log(JSON.stringify(spec, null, 2));
       done();
     });
   });
-
 
 });
