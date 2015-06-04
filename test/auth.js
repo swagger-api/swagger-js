ce87fe02ab2c3cae997cf569fa5faeb022f8e6f3
Josh Ponelat <josh@ponelat.com>
2015-06-04 12:45:49 +0200

    Refactor/add tests for lib/auth
    options.authorizations can now be set in the contructor, and in
    initialize.
    
    lib/auth tests now use nock, instead of a test server.

--- test/auth.js
+++ test/auth.js
@@ -1,38 +1,78 @@
-/* global after, before, describe, it */
+/* global  beforeEach, describe, it */
 
 'use strict';
 
-var expect = require('expect');
-var mock = require('./mock');
-var swagger = require('..');
-var sample, instance;
+var expect = require('chai').expect;
+var nock = require('nock');
+var petstore;
+var domain = 'http://example.com';
+
+var Swagger = require('..');
+var Auth = require('./auth');
+
+
+/**
+ * These are slightly different tests, as in they use http mocking rather than a test server.
+ * This is to showcase this alternative, in order to see if we can't migrate our other tests toward it
+ */
 
 describe('2.0 authorizations', function () {
-  before(function (done) {
-    mock.petstore(done, function (petstore, server){
-      sample = petstore;
-      instance = server;
-    });
-  });
 
-  after(function (done){
-    instance.close();
-    sample.clientAuthorizations.authz = {};
-    done();
+  beforeEach(function(done){
+    petstore = new Swagger({
+      spec: require('./spec/v2/petstore.json'),
+      success: done,
+      failure: function (err) {
+        throw err;
+      }
+    });
   });
 
   it('adds given hash to the auth object', function(){
 
     // I don't really care what it adds...
     var auth = {foo: 'bar'};
-    var client = new swagger('',{
+    var client = new Swagger('',{
       authorizations: {
         'someAuth': auth
       }
     });
 
-    expect(client.clientAuthorizations.authz.someAuth).toExist().toBe(auth);
+    expect(client.clientAuthorizations.authz.someAuth).to.equal(auth);
 
+  });
+
+  it('should have auth available when fetching the spec', function(done){
+    var uri = '/somespec.json';
+
+    // Mock out the request, will only allow one request
+    nock(domain)
+      .get(uri)
+      .reply(function () {
+        expect(this.req.headers).to.include.keys('swagger');
+        expect(this.req.headers.swagger).to.equal('awesome');
+        return {swagger: '2.0', title: 'test'}; // minimal, so that we don't break anything
+      });
+
+    var swag = new Swagger({
+      url: domain + uri,
+      authorizations: {
+        someAuth: function () {
+          this.headers.swagger  = 'awesome';
+        }
+      },
+      success: function () {
+        expect(swag.title).to.equal('test'); // Make sure we actually went through with the request
+        done();
+      },
+      failure: function (err) { throw err; }
+    });
+
+  });
+
+  it('should have clientAuthorizations instantiated before #initialize', function(){
+    var client = new Swagger(); // don't initialize here, no-args
+    expect(client.clientAuthorizations).to.respondTo('add'); // ie: it has an 'add' function
   });
 
   it('applies an api key to the query string', function () {
@@ -41,16 +81,14 @@ describe('2.0 authorizations', function () {
       responseContentType: 'application/json',
       mock: true
     };
-    var auth = new swagger.ApiKeyAuthorization('api_key', 'abc123', 'query');
+    var auth = new Swagger.ApiKeyAuthorization('api_key', 'abc123', 'query');
 
-    sample.clientAuthorizations.add('api_key', auth);
+    petstore.clientAuthorizations.add('api_key', auth);
 
-    var petApi = sample.pet;
+    var petApi = petstore.pet;
     var req = petApi.getPetById(params, opts);
 
-    expect(req.url).toBe('http://localhost:8000/v2/api/pet/1?api_key=abc123');
-
-    sample.clientAuthorizations.authz = {};
+    expect(req.url).to.equal('http://localhost:8000/v2/api/pet/1?api_key=abc123');
   });
 
   it('applies an api key as a header', function () {
@@ -59,17 +97,17 @@ describe('2.0 authorizations', function () {
       responseContentType: 'application/json',
       mock: true
     };
-    var auth = new swagger.ApiKeyAuthorization('api_key', 'abc123', 'header');
+    var auth = new Swagger.ApiKeyAuthorization('api_key', 'abc123', 'header');
 
-    sample.clientAuthorizations.add('api_key', auth);
+    petstore.clientAuthorizations.add('api_key', auth);
 
-    var petApi = sample.pet;
+    var petApi = petstore.pet;
     var req = petApi.getPetById(params, opts);
 
-    expect(req.url).toBe('http://localhost:8000/v2/api/pet/1');
-    expect(req.headers.api_key).toBe('abc123'); // jshint ignore:line
+    expect(req.url).to.equal('http://localhost:8000/v2/api/pet/1');
+    expect(req.headers.api_key).to.equal('abc123'); // jshint ignore:line
 
-    sample.clientAuthorizations.authz = {};
+    petstore.clientAuthorizations.authz = {};
   });
 
   it('does not apply security where it is explicitly not required', function () {
@@ -78,14 +116,14 @@ describe('2.0 authorizations', function () {
       responseContentType: 'application/json',
       mock: true
     };
-    var auth = new swagger.ApiKeyAuthorization('api_key', 'abc123', 'header');
+    var auth = new Swagger.ApiKeyAuthorization('api_key', 'abc123', 'header');
 
-    sample.clientAuthorizations.add('api_key', auth);
+    petstore.clientAuthorizations.add('api_key', auth);
 
-    var userApi = sample.user;
+    var userApi = petstore.user;
     var req = userApi.loginUser(params, opts);
 
-    expect(req.headers.api_key).toBe(undefined); // jshint ignore:line
+    expect(req.headers.api_key).to.equal(undefined); // jshint ignore:line
   });
 
 });
