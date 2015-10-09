50d4a68b783ca5a966249334ad4408bd7836a921
Tony Tam <fehguy@gmail.com>
2015-10-08 23:21:15 -0700

    rebuilt, bumped version

--- browser/swagger-client.min.js
+++ browser/swagger-client.min.js
@@ -1,6 +1,6 @@
 /**
  * swagger-client - swagger-client is a javascript client for use with swaggering APIs.
- * @version v2.1.5
+ * @version v2.1.6
  * @link http://swagger.io
  * @license Apache-2.0
  */
