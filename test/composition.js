f4622dd8324afd93b1540ade417a830ecb543855
Yuval Kogman <nothingmuch@woobling.org>
2015-10-29 10:21:18 +0000

    Also merge in top level fields in allOf composition

--- test/composition.js
+++ test/composition.js
@@ -69,6 +69,7 @@ describe('swagger resolver', function () {
           } ]
         },
         Monster: {
+          description: 'useful information',
           allOf : [
             {
               $ref: '#/definitions/Ghoul'
@@ -84,6 +85,7 @@ describe('swagger resolver', function () {
           ]
         },
         Ghoul: {
+          description: 'a ghoul',
           required: [ 'fangs' ],
           properties: {
             fangs: {
@@ -113,6 +115,8 @@ describe('swagger resolver', function () {
       expect(properties.hasScales['x-resolved-from']).toBe('self');
       test.undefined(spec.definitions.Animal.properties.firstName);
       expect(spec.definitions.Human.example).toBe('this is example');
+      expect(spec.definitions.Ghoul.description).toBe('a ghoul');
+      expect(spec.definitions.Monster.description).toBe('useful information');
       done();
     });
   });
