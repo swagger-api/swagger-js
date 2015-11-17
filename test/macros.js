a5967442fcd4b227a1dafefe262ab9607d9c0833
Tony Tam <fehguy@gmail.com>
2015-11-17 10:13:18 -0800

    added test for #612

---
+++ test/macros.js
@@ -0,0 +1,53 @@
+/* global describe, it */
+
+'use strict';
+
+var _ = require('lodash-compat');
+var expect = require('expect');
+var petstoreRaw = require('./spec/v2/petstore.json');
+var SwaggerClient = require('..');
+
+/* jshint ignore:start */
+var mock = require('./mock');
+var instance;
+/* jshint ignore:end */
+
+describe('client macros', function () {
+    /* jshint ignore:start */
+    before(function (done) {
+        mock.petstore(done, function (petstore, server){
+            instance = server;
+        });
+    });
+
+    after(function (done){
+        instance.close();
+        done();
+    });
+
+    it('tests the parameter macro per #612', function(done) {
+        var macros = {
+            parameter: function (operation, parameter) {
+                if(parameter.name === 'petId') {
+                    return '100';
+                }
+                return parameter.default;
+            },
+            modelProperty: function (property) {
+                return property.default;
+            }
+        };
+
+        new SwaggerClient({
+            url: 'http://petstore.swagger.io/v2/swagger.json',
+            usePromise: true,
+            parameterMacro: macros.parameter
+        }).then(function(client) {
+            var parameters = client.pet.apis.getPetById.parameters;
+            expect(parameters[0].default).toBe('100');
+            done();
+        }).catch(function(exception) {
+            done(exception);
+        });
+    });
+});
