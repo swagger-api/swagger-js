7f2e9f9ad2306e98c8df7a90c30e9ddf9201a0f8
Tony Tam <fehguy@gmail.com>
2015-11-16 21:32:22 -0800

    rebuilt

--- test/client.js
+++ test/client.js
@@ -324,7 +324,7 @@ describe('SwaggerClient', function () {
     });
   });
 
-  it('should should use a responseInterceptor', function(done) {
+  it('should use a responseInterceptor', function(done) {
     var responseInterceptor = {
       apply: function(data) {
         data.url = 'foo/bar';
