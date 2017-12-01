7405861e90812ac6352e7a659615d8479d57ceb6
Kyle Shockey <shockey@users.noreply.github.com>
2017-11-30 22:51:25 -0800

    Remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-const Swagger = require("./dist")
-
-const spec = JSON.parse(fs.readFileSync(__dirname + "/api-docs.json"))
-
-Swagger({ spec }).then((client) => {
-  console.log(client)
-  process.exit(0)
-})
