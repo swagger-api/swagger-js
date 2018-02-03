652467d11ca21da3dd0add07311565bd7877a9a3
Helder Sepulveda <heldersepu@gmail.com>
2018-02-03 13:46:10 -0500

    Trim trailing spaces

--- src/specmap/helpers.js
+++ src/specmap/helpers.js
@@ -17,7 +17,7 @@ const freelyNamedStrParents = [
 
 export function isFreelyNamed(parent) {
   const parentKey = parent[parent.length - 1]
-  const parentStr = parent.join('/')  
+  const parentStr = parent.join('/')
   return (
     (freelyNamedKeyParents.indexOf(parentKey) > -1) ||
     (freelyNamedStrParents.indexOf(parentStr) > -1)
