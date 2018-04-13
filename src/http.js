b2cf0737e60a0635a58832cbcfe5c8c310a6821d
kyle <shockey@users.noreply.github.com>
2018-04-13 14:30:01 -0700

    fix: http error handling regression (#1285)
    * tests: add failing test case provided in ticket
    * fix: throw original fetch error if we don't have any response data

--- src/http.js
+++ src/http.js
@@ -46,6 +46,11 @@ export default async function http(url, request = {}) {
     }
   }
   catch (resError) {
+    if (!res) {
+      // res is completely absent, so we can't construct our own error
+      // so we'll just throw the error we got
+      throw resError
+    }
     const error = new Error(res.statusText)
     error.statusCode = error.status = res.status
     error.responseError = resError
