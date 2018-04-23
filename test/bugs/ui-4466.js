4ebc16d7aa1ab59b9f66395e36fb7666ce98a7a9
Kyle Shockey <shockey@users.noreply.github.com>
2018-04-22 23:23:36 -0700

    fix: normalization regression

---
+++ test/bugs/ui-4466.js
@@ -0,0 +1,122 @@
+// https://github.com/swagger-api/swagger-ui/issues/4466
+// https://github.com/swagger-api/swagger-ui/issues/4467
+
+import expect, {createSpy, spyOn} from 'expect'
+import resolveSubtree from '../../src/subtree-resolver'
+
+const spec = {
+  swagger: '2.0',
+  info: {
+    version: 'v1',
+    title: 'Foo'
+  },
+  basePath: '/v1/foo',
+  produces: [
+    'application/json'
+  ],
+  parameters: {
+    testHeader: {
+      name: 'test-header',
+      description: 'some request header',
+      type: 'string',
+      in: 'header',
+      required: false
+    }
+  },
+  paths: {
+    '/': {
+      parameters: [
+        {
+          $ref: '#/parameters/testHeader'
+        }
+      ],
+      get: {
+        responses: {
+          200: {
+            description: 'Successful response',
+            schema: {
+              type: 'object',
+              properties: {
+                bar: {
+                  type: 'string'
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+it('should resolve test case from UI-4466 and UI-4467 correctly', async function () {
+  const res = await resolveSubtree(spec, [])
+
+  expect(res).toEqual({
+    errors: [],
+    spec: {
+      swagger: '2.0',
+      $$normalized: true,
+      basePath: '/v1/foo',
+      info: {
+        title: 'Foo',
+        version: 'v1',
+      },
+      parameters: {
+        testHeader: {
+          description: 'some request header',
+          in: 'header',
+          name: 'test-header',
+          required: false,
+          type: 'string',
+        },
+      },
+      paths: {
+        '/': {
+          get: {
+            parameters: [
+              {
+                $$ref: '#/parameters/testHeader',
+                description: 'some request header',
+                in: 'header',
+                name: 'test-header',
+                required: false,
+                type: 'string',
+              },
+            ],
+            produces: [
+              'application/json',
+            ],
+            responses: {
+              200: {
+                description: 'Successful response',
+                schema: {
+                  properties: {
+                    bar: {
+                      type: 'string',
+                    },
+                  },
+                  type: 'object',
+                },
+              },
+            },
+          },
+          parameters: [
+            {
+              $$ref: '#/parameters/testHeader',
+              description: 'some request header',
+              in: 'header',
+              name: 'test-header',
+              required: false,
+              type: 'string',
+            },
+          ],
+        },
+      },
+      produces: [
+        'application/json',
+      ]
+    }
+  })
+})
