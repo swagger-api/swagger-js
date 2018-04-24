f5dd332e57eff41caf20b0a9dd964034fc5aaf5f
Kyle Shockey <shockey@users.noreply.github.com>
2018-04-23 23:22:24 -0700

    fix: edge case where invalid parameter locks up normalizer

--- test/bugs/ui-4466.js
+++ test/bugs/ui-4466.js
@@ -120,3 +120,103 @@ it('should resolve test case from UI-4466 and UI-4467 correctly', async function
     }
   })
 })
+
+it('should resolve modified test case where parameter is badly formatted', async function () {
+  const invalidSpec = {
+    swagger: '2.0',
+    info: {
+      version: 'v1',
+      title: 'Foo'
+    },
+    basePath: '/v1/foo',
+    produces: [
+      'application/json'
+    ],
+    parameters: {
+      testHeader: {
+        name: 'test-header',
+        description: 'some request header',
+        type: 'string',
+        in: 'header',
+        required: false
+      }
+    },
+    paths: {
+      '/': {
+        parameters: [
+          {}
+        ],
+        get: {
+          responses: {
+            200: {
+              description: 'Successful response',
+              schema: {
+                type: 'object',
+                properties: {
+                  bar: {
+                    type: 'string'
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const res = await resolveSubtree(invalidSpec, [])
+
+  expect(res).toEqual({
+    errors: [],
+    spec: {
+      swagger: '2.0',
+      $$normalized: true,
+      basePath: '/v1/foo',
+      info: {
+        title: 'Foo',
+        version: 'v1',
+      },
+      parameters: {
+        testHeader: {
+          description: 'some request header',
+          in: 'header',
+          name: 'test-header',
+          required: false,
+          type: 'string',
+        },
+      },
+      paths: {
+        '/': {
+          get: {
+            parameters: [
+              {},
+            ],
+            produces: [
+              'application/json',
+            ],
+            responses: {
+              200: {
+                description: 'Successful response',
+                schema: {
+                  properties: {
+                    bar: {
+                      type: 'string',
+                    },
+                  },
+                  type: 'object',
+                },
+              },
+            },
+          },
+          parameters: [
+            {},
+          ],
+        },
+      },
+      produces: [
+        'application/json',
+      ]
+    }
+  })
+})
