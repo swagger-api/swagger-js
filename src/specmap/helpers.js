813e4d8c5936482bc792e4cb7ad14cbcdbb72566
Kyle Shockey <shockey@users.noreply.github.com>
2018-07-17 16:54:03 -0700

    fix: exempt schema example children from $ref resolution

--- src/specmap/helpers.js
+++ src/specmap/helpers.js
@@ -3,9 +3,12 @@
 // if/when another helper is added to this file,
 // please remove the eslint override and this comment!
 
+// This will match if the direct parent's key exactly matches an item.
 const freelyNamedKeyParents = [
   'properties',
 ]
+
+// This will match if the joined parent path exactly matches an item.
 const freelyNamedPaths = [
   // Swagger 2.0
   'definitions',
@@ -17,14 +20,25 @@ const freelyNamedPaths = [
   'components/schemas',
   'components/responses',
   'components/parameters',
-  'components/securitySchemes',
+  'components/securitySchemes'
 ]
 
-export function isFreelyNamed(parent) {
-  const parentKey = parent[parent.length - 1]
-  const parentStr = parent.join('/')
-  return (
+// This will match if any of these items are substrings of the joined
+// parent path.
+//
+// Warning! These are powerful. Beware of edge cases.
+const freelyNamedAncestors = [
+  'schema/example'
+]
+
+export function isFreelyNamed(parentPath) {
+  const parentKey = parentPath[parentPath.length - 1]
+  const parentStr = parentPath.join('/')
+
+  const res = (
     (freelyNamedKeyParents.indexOf(parentKey) > -1) ||
-    (freelyNamedPaths.indexOf(parentStr) > -1)
+    (freelyNamedPaths.indexOf(parentStr) > -1) ||
+    (freelyNamedAncestors.some(el => parentStr.indexOf(el) > -1))
   )
+  return res
 }
