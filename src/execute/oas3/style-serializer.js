7f2f6b707993e6cd8c29ded19aec35a3ea37f33c
kyle <shockey@users.noreply.github.com>
2018-09-14 20:48:37 -0700

    linter fixes

--- src/execute/oas3/style-serializer.js
+++ src/execute/oas3/style-serializer.js
@@ -33,7 +33,7 @@ export function encodeDisallowedCharacters(str, {escape} = {}, parse) {
     }
 
     const encoded = (toUTF8Bytes(char) || [])
-      .map(byte => ("0"+byte.toString(16).toUpperCase()).slice(-2))
+      .map(byte => (`0${byte.toString(16).toUpperCase()).slice(-2)}`)
       .map(encodedByte => `%${encodedByte}`)
       .join('')
 
