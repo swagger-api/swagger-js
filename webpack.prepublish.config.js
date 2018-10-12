8c43253aeba79c9186531d990f74f030b723f1ec
Sebastien Perez <sebastian.perezvasseur@gmail.com>
2018-10-12 19:23:42 +0200

    New bundle option that creates an index.js file in umd. Necessary to have the library working directly from unpkg

---
+++ webpack.prepublish.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const deepMerge = require('deepmerge')
+const webpackConfig = require('./webpack.common.js')
+
+module.exports = deepMerge(
+  webpackConfig, {
+    externals: (ctx, req, next) => {
+      next(null, false)
+    },
+
+    output: {
+      path: path.join(__dirname, 'umd'),
+      library: 'SwaggerClient',
+      libraryTarget: 'umd',
+      filename: 'index.js'
+    },
+
+  }
+)
