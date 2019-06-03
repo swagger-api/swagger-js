ef5232f752b7dafc263b98b22e6d646edfea65dd
Kyle Shockey <shockey@users.noreply.github.com>
2019-06-02 19:54:24 -0700

    generateAbsoluteRefPatches in allOf

--- src/specmap/helpers.js
+++ src/specmap/helpers.js
@@ -1,7 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-//
-// if/when another helper is added to this file,
-// please remove the eslint override and this comment!
+import traverse from "traverse"
+import URL from "url"
 
 // This will match if the direct parent's key exactly matches an item.
 const freelyNamedKeyParents = [
@@ -52,4 +50,26 @@ export function isFreelyNamed(parentPath) {
     (freelyNamedPaths.indexOf(parentStr) > -1) ||
     (freelyNamedAncestors.some(el => parentStr.indexOf(el) > -1))
   )
+}
+
+export function generateAbsoluteRefPatches(obj, fullPath, specmap) {
+  const patches = []
+
+  traverse(obj).forEach(function () {
+    if (this.key === "$ref" || this.key === "$$ref") {
+      const [urlPart, fragmentPart] = this.node.split("#")
+      const nodePath = fullPath.concat(this.path)
+      const collapsedNodePath = fullPath
+        // slice off `["allOf", n]`, since the allOf will be collapsed when this patch is applied
+        .slice(0, -2)
+        .concat(this.path)
+      const { baseDoc } = specmap.getContext(nodePath)
+      const newRefUrlPart = URL.resolve(urlPart, baseDoc)
+
+      patches.push(specmap.replace(collapsedNodePath, `${newRefUrlPart}#${fragmentPart}`))
+      debugger
+    }
+  })
+
+  return patches
 }
--- src/specmap/lib/all-of.js
+++ src/specmap/lib/all-of.js
@@ -1,4 +1,4 @@
-import {isFreelyNamed} from '../helpers'
+import {isFreelyNamed, generateAbsoluteRefPatches} from '../helpers'
 
 export default {
   key: 'allOf',
@@ -34,7 +34,12 @@ export default {
     originalDefinitionObj = Object.assign({}, originalDefinitionObj)
     delete originalDefinitionObj.allOf
 
-    const allOfPatches = [specmap.replace(parent, {})].concat(val.map((toMerge, index) => {
+    const patches = []
+
+    // remove existing content
+    patches.push(specmap.replace(parent, {}))
+
+    val.forEach((toMerge, i) => {
       if (!specmap.isObject(toMerge)) {
         if (alreadyAddError) {
           return null
@@ -43,21 +48,65 @@ export default {
 
         const err = new TypeError('Elements in allOf must be objects')
         err.fullPath = fullPath // This is an array
-        return err
+        return patches.push(err)
       }
 
-      return specmap.mergeDeep(parent, toMerge)
-    }))
+      // Deeply merge the member's contents onto the parent location
+      patches.push(specmap.mergeDeep(parent, toMerge))
+
+      // Generate patches that migrate $ref values based on ContextTree information
+      patches.push(...generateAbsoluteRefPatches(toMerge, fullPath.concat([i]), specmap))
+    })
 
     // Merge back the values from the original definition
-    allOfPatches.push(specmap.mergeDeep(parent, originalDefinitionObj))
+    patches.push(specmap.mergeDeep(parent, originalDefinitionObj))
 
     // If there was not an original $$ref value, make sure to remove
     // any $$ref value that may exist from the result of `allOf` merges
     if (!originalDefinitionObj.$$ref) {
-      allOfPatches.push(specmap.remove([].concat(parent, '$$ref')))
+      patches.push(specmap.remove([].concat(parent, '$$ref')))
     }
 
-    return allOfPatches
+    return patches
   }
 }
+
+// ## Problem
+// If an allOf member's content is provided by remote $ref, any remaining $ref
+// values within the content will still be expressed from the point of view of
+// the remote document. Usually this is handled by Specmap's ContextTree, but
+// allOf causes the ContextTree to be out of sync, because the ContextTree can't
+// track the original source of each member's contents once it has all been
+// collapsed into one object.
+//
+// ## Example
+// 
+// (a.yaml)
+// myObj:
+//   allOf:
+//   - $ref: "./b.yaml#one"
+//
+// (b.yaml)
+// one:
+//   $ref: "#/two"
+// two:
+//   hello: "world"
+//
+// When the $ref in `a.yaml` is resolved, ContextTree knows that ["myObj",
+// "allOf", 0] is coming from `b.yaml`, but once the allOf plugin collapses the
+// allOf array into a single result object at ["myObj"], that context is lost.
+// 
+// As a result, the resolver incorrectly reads the `#/two` $ref as a local ref
+// within `a.yaml`, and an error is generated.
+// 
+// Furthermore, even if ContextTree could keep up with each $ref, we'd still be
+// *possibly* handing back invalid $ref values to the user, because the resolver
+// is sometimes throttled by path restriction and depth limitations.
+// 
+// ## Solution
+// Generate a set of patches that will rewrite any $refs within an allOf member
+// to be valid within their new position in the root document, using the
+// existing ContentTree data as a basis for rewriting each reference.
+// 
+// Essentially: persist the ContextTree information by writing it into the $ref.
+
