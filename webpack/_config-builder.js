397fc7af59eff1bf1608b580049dddf61da82b96
kyle <shockey@users.noreply.github.com>
2019-07-18 00:28:35 -0500

    housekeeping: webpack@4 + babel@7 (via #1455)
    * prettier
    
    * babel@7
    
    * jest@24
    
    * install @babel/register
    
    * webpack@4
    
    * use new scripts
    
    * remove unused PARAMETER_BUILDERS assignment
    
    this was from the original 3.0.0 commit, and has never worked!
    
    * standardize webpack umd flag
    
    * fix test setup config pointer
    
    * update babel runtime pkg name
    
    * use default export in Webpack
    
    * drop babel-plugin-add-module-exports

---
+++ webpack/_config-builder.js
@@ -0,0 +1,83 @@
+/**
+ * @prettier
+ */
+
+import path from 'path'
+import deepExtend from 'deep-extend'
+import TerserPlugin from 'terser-webpack-plugin'
+import nodeExternals from 'webpack-node-externals'
+
+const baseRules = [
+  {
+    test: /\.js/,
+    loader: 'babel-loader?retainLines=true',
+    exclude: [/node_modules/],
+  },
+]
+
+export default function buildConfig(
+  {minimize = true, mangle = true, sourcemaps = true, includeDependencies = true, umd = false},
+  customConfig
+) {
+  const plugins = []
+
+  const completeConfig = deepExtend(
+    {},
+    {
+      mode: 'production',
+
+      entry: {
+        'swagger-client': ['./src/index.js'],
+      },
+
+      devtool: 'source-map',
+
+      output: {
+        path: path.join(__dirname, '../dist'),
+        library: 'SwaggerClient',
+        libraryExport: 'default',
+        filename: 'index.js',
+        ...(umd
+          ? {
+            libraryTarget: 'umd',
+            globalObject: 'this',
+          }
+          : {
+            libraryTarget: 'commonjs2',
+          }),
+      },
+
+      externals: includeDependencies ? [] : [nodeExternals()],
+
+      module: {
+        rules: baseRules,
+      },
+
+      resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.json'],
+      },
+
+      optimization: {
+        minimize: !!minimize,
+        minimizer: [
+          compiler =>
+            new TerserPlugin({
+              cache: true,
+              sourceMap: sourcemaps,
+              terserOptions: {
+                mangle: !!mangle,
+              },
+            }).apply(compiler),
+        ],
+      },
+    },
+
+    customConfig
+  )
+
+  // deepExtend mangles Plugin instances, this doesn't
+  completeConfig.plugins = plugins.concat(customConfig.plugins || [])
+
+  return completeConfig
+}
