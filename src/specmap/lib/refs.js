8e8e4ae1fb8a30cc5f8827c52a734c4f0dc47c27
Kyle Shockey <shockey@users.noreply.github.com>
2019-10-09 12:35:08 -0700

    fix import path

--- src/specmap/lib/refs.js
+++ src/specmap/lib/refs.js
@@ -5,7 +5,7 @@ import url from 'url'
 import lib from '../lib'
 import createError from '../lib/create-error'
 import {isFreelyNamed, absolutifyPointer} from '../helpers'
-import {ACCEPT_HEADER_VALUE_FOR_DOCUMENTS} from '../constants'
+import {ACCEPT_HEADER_VALUE_FOR_DOCUMENTS} from '../../constants'
 
 const ABSOLUTE_URL_REGEXP = new RegExp('^([a-z]+://|//)', 'i')
 
