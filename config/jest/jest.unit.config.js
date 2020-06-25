b65bbb7202d4719929dc1e46531955aef8b8e047
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-25 14:40:16 +0200

    chore: config

--- config/jest/jest.unit.config.js
+++ config/jest/jest.unit.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
+  rootDir: path.join(__dirname, '..', '..'),
   testEnvironment: 'node',
-  testMatch: ['**/test/*.js?(x)', '**/test/**/*.js?(x)'],
+  testMatch: ['**/test/*.js', '**/test/**/*.js'],
   setupFilesAfterEnv: ['<rootDir>/test/jest.setup.js'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
