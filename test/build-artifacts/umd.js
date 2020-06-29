135a03fe94d55046d86947f605ebb71d43736785
Vladimir Gorej <vladimir.gorej@gmail.com>
2020-06-29 12:40:52 +0200

    test(build-artifacts): test non-minized UMD artifacts too
    Refs #1589

--- test/build-artifacts/umd.js
+++ test/build-artifacts/umd.js
@@ -1,4 +1,5 @@
-import SwaggerClient from '../../dist/swagger-client.browser.min';
+import SwaggerClient from '../../dist/swagger-client.browser';
+import SwaggerClientMin from '../../dist/swagger-client.browser.min';
 
 describe('webpack browser umd build', () => {
   test('should export a function', () => {
@@ -36,6 +37,61 @@ describe('webpack browser umd build', () => {
     };
 
     const res = await SwaggerClient.resolve({
+      spec,
+      allowMetaPatches: false,
+    });
+
+    expect(res.errors).toEqual([]);
+    expect(res.spec).toEqual({
+      a: {
+        value: 1234,
+      },
+      b: {
+        value: 1234,
+      },
+      c: {
+        value: 1234,
+      },
+    });
+  });
+});
+
+describe('webpack browser umd minimized build', () => {
+  test('should export a function', () => {
+    expect(SwaggerClientMin).toBeInstanceOf(Function);
+  });
+
+  test('should export helpers attached to the default export', () => {
+    expect(SwaggerClientMin.http).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.makeHttp).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.resolve).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.resolveSubtree).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.execute).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.serializeRes).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.serializeHeaders).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.clearCache).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.makeApisTagOperation).toBeInstanceOf(Function);
+    expect(SwaggerClientMin.buildRequest).toBeInstanceOf(Function);
+    expect(Object.keys(SwaggerClientMin.helpers)).toContain('opId');
+    expect(SwaggerClientMin.getBaseUrl).toBeInstanceOf(Function);
+  });
+
+  test('should be able to resolve things when minified', async () => {
+    const spec = {
+      a: {
+        $ref: '#/b',
+      },
+      b: {
+        value: {
+          $ref: '#/c/value',
+        },
+      },
+      c: {
+        value: 1234,
+      },
+    };
+
+    const res = await SwaggerClientMin.resolve({
       spec,
       allowMetaPatches: false,
     });
