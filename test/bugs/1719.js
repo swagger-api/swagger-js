ce42e691fd262361911da6d01e01e7bc4fc17af3
Timothy Lai <timothy.lai@gmail.com>
2020-09-18 14:32:59 -0700

    refactor: simplify test and yaml files

--- test/bugs/1719.js
+++ test/bugs/1719.js
@@ -7,46 +7,28 @@ import jsYaml from 'js-yaml';
 import resolveSubtree from '../../src/subtree-resolver';
 
 const testDoc = jsYaml.safeLoad(
-  fs.readFileSync(path.join('test', 'data', 'swos-123-ref-object-reference.yaml'), 'utf8')
+  fs.readFileSync(path.join('test', 'data', 'issue-1719-ref-object-reference.yaml'), 'utf8')
 );
 
-test('1719: $ref object should not be treated as a reference (string)', async () => {
+test('#1719: $ref object should not be treated as a reference (string)', async () => {
   const specPath = ['paths', '/throwError', 'post'];
   const res = await resolveSubtree(testDoc, specPath);
 
   expect(res.errors.length).toEqual(0);
-
-  expect(res.spec).toEqual(
-    expect.objectContaining({
-      requestBody: expect.objectContaining({
-        content: expect.objectContaining({
-          'application/json': expect.objectContaining({
-            schema: expect.objectContaining({
-              properties: expect.objectContaining({
-                references: expect.objectContaining({
-                  properties: expect.objectContaining({
-                    billingDetails: expect.objectContaining({
-                      items: expect.objectContaining({
-                        properties: expect.objectContaining({
-                          contact: expect.objectContaining({
-                            anyOf: expect.arrayContaining([
-                              expect.objectContaining({
-                                properties: expect.objectContaining({
-                                  $ref: { type: 'string' },
-                                }),
-                              }),
-                            ]),
-                          }),
-                        }),
-                      }),
-                    }),
-                  }),
-                }),
-              }),
-            }),
-          }),
-        }),
-      }),
-    })
+  expect(res.spec).toHaveProperty(
+    [
+      'requestBody',
+      'content',
+      'application/json',
+      'schema',
+      'properties',
+      'propertyOne',
+      'items',
+      'properties',
+      'propertyTwo',
+      'properties',
+      '$ref',
+    ],
+    { type: 'string' }
   );
 });
