c366493ded6fb81b37cf887c9b08a54e34bd5875
Tim Lai <timothy.lai@gmail.com>
2020-09-22 10:14:48 -0700

    fix: check node type before treating object as reference (#1719) (#1720)
    ref #1719

---
+++ test/bugs/1719.js
@@ -0,0 +1,34 @@
+// https://github.com/swagger-api/swagger-js/issues/1719
+
+import path from 'path';
+import fs from 'fs';
+import jsYaml from 'js-yaml';
+
+import resolveSubtree from '../../src/subtree-resolver';
+
+const testDoc = jsYaml.safeLoad(
+  fs.readFileSync(path.join('test', 'data', 'issue-1719-ref-object-reference.yaml'), 'utf8')
+);
+
+test('#1719: $ref object should not be treated as a reference (string)', async () => {
+  const specPath = ['paths', '/throwError', 'post'];
+  const res = await resolveSubtree(testDoc, specPath);
+
+  expect(res.errors.length).toEqual(0);
+  expect(res.spec).toHaveProperty(
+    [
+      'requestBody',
+      'content',
+      'application/json',
+      'schema',
+      'properties',
+      'propertyOne',
+      'items',
+      'properties',
+      'propertyTwo',
+      'properties',
+      '$ref',
+    ],
+    { type: 'string' }
+  );
+});
