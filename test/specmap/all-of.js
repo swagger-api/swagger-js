swagger-api/swagger-js

commit 2f5bb86c9b88193477ca8ded3a136976a8ea0800

Fix and test for swagger-ui #3328: https://github.com/swagger-api/swagger-ui/issues/3328. Added manual logic to merge arrays after calling deepExtend within `mergeDeep` function

src/specmap/lib/index.js
@@ -44,7 +44,19 @@ function applyPatch(obj, patch, opts) {
   else if (patch.op === 'mergeDeep') {
     const valPatch = _get(patch.path)
     jsonPatch.apply(obj, [valPatch])
+    const origValPatchValue = Object.assign({}, valPatch.value)
     deepExtend(valPatch.value, patch.value)
+
+    // deepExtend doesn't merge arrays, so we will do it manually
+    for ( let prop in patch.value ) {
+      if ( patch.value.hasOwnProperty(prop) ) {
+        const propVal = patch.value[prop]
+        if ( Array.isArray(propVal) ) {
+          let existing = origValPatchValue[prop] || []
+          valPatch.value[prop] = existing.concat( propVal )
+        }
+      }
+    }
   }
   else {
     jsonPatch.apply(obj, [patch])

test/specmap/all-of.js
@@ -173,6 +173,51 @@ describe('allOf', function () {
       })
     })
   })
+
+  it('merges arrays inside of an `allOf`', function() {
+    return mapSpec({
+      plugins: [plugins.refs, plugins.allOf],
+      showDebug: true,
+      spec: {
+        definitions: {
+          one: {
+            allOf: [
+              {
+                $ref: '#/definitions/two'
+              },
+              {
+                type: 'object',
+                required: ['a', 'b']
+              }
+            ]
+          },
+          two: {
+            allOf: [
+              {
+                type: 'object',
+                required: ['c', 'd']
+              }
+            ]
+          }
+        }
+      }
+    }).then((res) => {
+      expect(res.errors).toEqual([])
+      expect(res.spec).toEqual({
+        definitions: {
+          one: {
+            type: 'object',
+            required: ['c', 'd', 'a', 'b']
+          },
+          two: {
+            type: 'object',
+            required: ['c', 'd']
+          }
+        },
+      })
+    })
+  })
+
   // TODO: this needs to get fixed
   it.skip('should handle case, with an `allOf` referencing an `allOf` ', function () {
     return mapSpec({

commit 538b18030e9cd5f3950ea323bd3e4862ae061105

fix: deep array merge
deep-extend does not support array merge. There was special code added
to merge top-level arrays, but that was a shallow merge.

Use deepmerge instead of deep-extend to merge arrays also. Default merge
settings seem to work well - all tests pass.

Extend all-of merge test case based on
https://github.com/swagger-api/swagger-ui/issues/7618

Fixes: 2f5bb86c9b88 ("Fix and test for swagger-ui #3328: https://github.com/swagger-api/swagger-ui/issues/3328. Added manual logic to merge arrays after calling deepExtend within `mergeDeep` function")

src/specmap/lib/index.js
@@ -1,6 +1,5 @@
 import * as jsonPatch from 'fast-json-patch';
-import deepExtend from 'deep-extend';
-import cloneDeep from 'lodash/cloneDeep';
+import deepmerge from 'deepmerge';
 
 export default {
   add,
@@ -40,42 +39,8 @@ function applyPatch(obj, patch, opts) {
     jsonPatch.applyPatch(obj, [replace(patch.path, newValue)]);
   } else if (patch.op === 'mergeDeep') {
     const currentValue = getInByJsonPath(obj, patch.path);
-
-    // Iterate the properties of the patch
-    // eslint-disable-next-line no-restricted-syntax, guard-for-in
-    for (const prop in patch.value) {
-      const propVal = patch.value[prop];
-      const isArray = Array.isArray(propVal);
-      if (isArray) {
-        // deepExtend doesn't merge arrays, so we will do it manually
-        const existing = currentValue[prop] || [];
-        currentValue[prop] = existing.concat(propVal);
-      } else if (isObject(propVal) && !isArray) {
-        // If it's an object, iterate it's keys and merge
-        // if there are conflicting keys, merge deep, otherwise shallow merge
-        let currentObj = { ...currentValue[prop] };
-        // eslint-disable-next-line no-restricted-syntax
-        for (const key in propVal) {
-          if (Object.prototype.hasOwnProperty.call(currentObj, key)) {
-            // if there is a single conflicting key, just deepExtend the entire value
-            // and break from the loop (since all future keys are also merged)
-            // We do this because we can't deepExtend two primitives
-            // (currentObj[key] & propVal[key] may be primitives).
-            //
-            // we also deeply assign here, since we aren't in control of
-            // how deepExtend affects existing nested objects
-            currentObj = deepExtend(cloneDeep(currentObj), propVal);
-            break;
-          } else {
-            Object.assign(currentObj, { [key]: propVal[key] });
-          }
-        }
-        currentValue[prop] = currentObj;
-      } else {
-        // It's a primitive, just replace existing
-        currentValue[prop] = propVal;
-      }
-    }
+    const newValue = deepmerge(currentValue, patch.value);
+    obj = jsonPatch.applyPatch(obj, [replace(patch.path, newValue)]).newDocument;
   } else if (patch.op === 'add' && patch.path === '' && isObject(patch.value)) {
     // { op: 'add', path: '', value: { a: 1, b: 2 }}
     // has no effect: json patch refuses to do anything.

test/specmap/all-of.js
@@ -436,7 +436,7 @@ describe('allOf', () => {
     });
   });
 
-  test('merges arrays inside of an `allOf`', () =>
+  test('deepmerges arrays inside of an `allOf`', () =>
     mapSpec({
       plugins: [plugins.refs, plugins.allOf],
       showDebug: true,
@@ -450,6 +450,12 @@ describe('allOf', () => {
               {
                 type: 'object',
                 required: ['a', 'b'],
+                properties: {
+                  nested: {
+                    type: 'object',
+                    required: ['e'],
+                  },
+                },
               },
             ],
           },
@@ -458,6 +464,12 @@ describe('allOf', () => {
               {
                 type: 'object',
                 required: ['c', 'd'],
+                properties: {
+                  nested: {
+                    type: 'object',
+                    required: ['f'],
+                  },
+                },
               },
             ],
           },
@@ -470,10 +482,22 @@ describe('allOf', () => {
           one: {
             type: 'object',
             required: ['c', 'd', 'a', 'b'],
+            properties: {
+              nested: {
+                type: 'object',
+                required: ['f', 'e'],
+              },
+            },
           },
           two: {
             type: 'object',
             required: ['c', 'd'],
+            properties: {
+              nested: {
+                type: 'object',
+                required: ['f'],
+              },
+            },
           },
         },
       });