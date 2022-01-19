swagger-api/swagger-js

commit 067229e8f7d6b55a68cb62278a662fc2a97c9f0a

refactor: replace lodash isObject/isPlainObject with alternatives (#2309)
Refs #2187

src/helpers.js
@@ -1,5 +1,3 @@
-import isObject from 'lodash/isObject';
-
 const toLower = (str) => String.prototype.toLowerCase.call(str);
 const escapeString = (str) => str.replace(/[^\w]/gi, '_');
 
@@ -136,7 +134,7 @@ export function normalizeSwagger(parsedSpec) {
   for (const pathName in paths) {
     const path = paths[pathName];
 
-    if (!isObject(path)) {
+    if (path == null || !['object', 'function'].includes(typeof path)) {
       continue; // eslint-disable-line no-continue
     }
 
@@ -145,7 +143,7 @@ export function normalizeSwagger(parsedSpec) {
     // eslint-disable-next-line no-restricted-syntax, guard-for-in
     for (const method in path) {
       const operation = path[method];
-      if (!isObject(operation)) {
+      if (path == null || !['object', 'function'].includes(typeof path)) {
         continue; // eslint-disable-line no-continue
       }
 

src/execute/index.js
@@ -1,7 +1,7 @@
 import getIn from 'lodash/get';
-import isPlainObject from 'lodash/isPlainObject';
 import url from 'url';
 import cookie from 'cookie';
+import { isPlainObject } from 'is-plain-object';
 
 import stockHttp, { mergeInQueryOrForm } from '../http/index.js';
 import createError from '../specmap/lib/create-error.js';

src/execute/oas3/build-request.js
@@ -1,7 +1,7 @@
 // This function runs after the common function,
 // `src/execute/index.js#buildRequest`
+import { isPlainObject } from 'is-plain-object';
 import get from 'lodash/get';
-import isPlainObject from 'lodash/isPlainObject';
 import btoa from 'btoa';
 
 export default function buildRequest(options, req) {

commit f43724667785f605d7e6260d3d15631f055d4c00

fix(normalizeSwagger): fix regression in swagger normalization
Regression was introduced in 067229e.

Refs https://github.com/swagger-api/swagger-ui/issues/7771

src/helpers.js
@@ -143,7 +143,7 @@ export function normalizeSwagger(parsedSpec) {
     // eslint-disable-next-line no-restricted-syntax, guard-for-in
     for (const method in path) {
       const operation = path[method];
-      if (path == null || !['object', 'function'].includes(typeof path)) {
+      if (operation == null || !['object', 'function'].includes(typeof operation)) {
         continue; // eslint-disable-line no-continue
       }
 