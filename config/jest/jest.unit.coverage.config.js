8f2145e621c87ca877cf35a42f0dad77e907cb52
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-01-18 11:35:24 +0100

    test: use original coverage thresholds

--- config/jest/jest.unit.coverage.config.js
+++ config/jest/jest.unit.coverage.config.js
@@ -6,14 +6,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.js'],
   coverageThreshold: {
     './src/': {
-      branches: 10,
-      functions: 10,
-      lines: 10,
-      statements: 10,
-      // branches: 87,
-      // functions: 91,
-      // lines: 90,
-      // statements: 90,
+      branches: 86,
+      functions: 91,
+      lines: 90,
+      statements: 89,
     },
   },
 };
