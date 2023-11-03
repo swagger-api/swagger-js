d45c9ab8703f0730a8ad0b83d8dc3c85c383fead
Vladimír Gorej <vladimir.gorej@smartbear.com>
2023-11-03 14:32:01 +0100

    fix(execute): handle Server Objects overrides for OpenAPI 2.0/3.0.x (#3221)
    This change is specific to OpenAPI 2.0 and OpenAPI 3.0.x.
    
    Refs #2967

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 export const ACCEPT_HEADER_VALUE_FOR_DOCUMENTS = 'application/json, application/yaml';
 
 export const DEFAULT_BASE_URL = 'https://swagger.io';
+
+export const DEFAULT_OPENAPI_3_SERVER = Object.freeze({ url: '/' });
