904ac5cd42e7e184eb088616acc29572a0cf61f6
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-05-08 16:45:09 +0200

    fix(security): fix polynomial regular expression (#3502)
    Refs #3501
    
    ---------
    
    Co-authored-by: Vladimír Gorej <vladimir.gorej@gmail.com>

--- src/execute/index.js
+++ src/execute/index.js
@@ -390,7 +390,7 @@ function buildOas3UrlWithContext(ourUrl = '', contextUrl = '') {
 
 function getVariableTemplateNames(str) {
   const results = [];
-  const re = /{([^}]+)}/g;
+  const re = /{([^}]{1,29})}/g;
   let text;
 
   // eslint-disable-next-line no-cond-assign
