dd20c573fa8fbc49f5539424f73c3d15681b1714
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-05-08 16:58:37 +0200

    fix(security): fix polynomial regular expression without limit length
    Refs #3501

--- src/execute/index.js
+++ src/execute/index.js
@@ -390,7 +390,7 @@ function buildOas3UrlWithContext(ourUrl = '', contextUrl = '') {
 
 function getVariableTemplateNames(str) {
   const results = [];
-  const re = /{([^}]{1,29})}/g;
+  const re = /\{([^{}]+)}|([^{}]+)/g;
   let text;
 
   // eslint-disable-next-line no-cond-assign
